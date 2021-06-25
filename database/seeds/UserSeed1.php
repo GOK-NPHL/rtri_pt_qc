15b90b4b7804568007fbca1f782a329012e0b910
duncan <duncanndiithi@yahoo.com>
2021-06-25 19:38:32 +0300

    add seed information

--- database/seeds/UserSeed1.php
+++ database/seeds/UserSeed1.php
@@ -13,7 +13,23 @@ class UserSeed extends Seeder
     public function run()
     {
         $data = array(
-            array('name' => 'duncan', 'email' => 'duncanndiithi@gmail.com', 'password' => '$2y$10$wCyQ7j2mwl.NGD3brp1RSuCo3nIv9b1pDO4Cb8v0xjmfBshm93bGm', 'created_at' => new \dateTime, 'updated_at' => new \dateTime),
+            array(
+                'name' => 'duncan',
+                'email' => 'duncanndiithi@gmail.com',
+                'password' => '$2y$10$wCyQ7j2mwl.NGD3brp1RSuCo3nIv9b1pDO4Cb8v0xjmfBshm93bGm',
+                'created_at' => new \dateTime,
+                'updated_at' => new \dateTime,
+                'user_type' => 'admin'
+            ),
+            array(
+                'name' => 'duncan',
+                'email' => 'duncanndiithi@yahoo.com',
+                'password' => '$2y$10$wCyQ7j2mwl.NGD3brp1RSuCo3nIv9b1pDO4Cb8v0xjmfBshm93bGm',
+                'created_at' => new \dateTime,
+                'updated_at' => new \dateTime,
+                'user_type' => 'participant'
+            )
+
         );
 
         $authObj = new User();
