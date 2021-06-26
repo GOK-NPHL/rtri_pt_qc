7d1f29cb36a3208088d5435b84cf133410d16e46
duncan <duncanndiithi@yahoo.com>
2021-06-27 00:01:16 +0300

    auth endpoints

--- routes/web.php
+++ routes/web.php
@@ -28,14 +28,14 @@ Route::get('index', [CustomAuthController::class, 'index'])->name('index');
 Route::get('login', [CustomAuthController::class, 'index'])->name('login');
 Route::get('admin-login', [CustomAuthController::class, 'adminLogin'])->name('admin-login');
 Route::get('participant-login', [CustomAuthController::class, 'getParticipantLoginPage'])->name('participant-login');
-Route::post('do-login', [CustomAuthController::class, 'doLogin'])->name('do-login');
+Route::post('admin-login', [CustomAuthController::class, 'doLogin'])->name('admin-login');
+Route::post('participant-login', [CustomAuthController::class, 'doLogin'])->name('participant-login');
 Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
 Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom');
-Route::get('logout', [CustomAuthController::class, 'signOut'])->name('signout');
+Route::get('logout', [CustomAuthController::class, 'signOut'])->name('logout');
 
 
-Route::get('/participant-home', 'QC\QCController@participant')->name('participant-home');
-Route::get('/admin-home', 'QC\QCController@admin')->name('admin-home');
+Route::get('participant-home', 'QC\QCController@participantHome')->name('participant-home');
+Route::get('admin-home', 'QC\QCController@adminHome')->name('admin-home')->middleware('admin');
 
-Route::get('/dashboard', 'Service\Rtri@dashboard')->name('dashboard');
-
+Route::get('dashboard', 'Service\Rtri@dashboard')->name('dashboard');
