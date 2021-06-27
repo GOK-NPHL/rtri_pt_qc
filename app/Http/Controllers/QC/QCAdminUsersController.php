de127819e41b5be4877db4c2c3578fac1ad58543
duncan <duncanndiithi@yahoo.com>
2021-06-27 16:07:13 +0300

    admin user controller

---
+++ app/Http/Controllers/QC/QCAdminUsersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\QC;
+
+use App\Admin;
+use App\Http\Controllers\Controller;
+use Exception;
+use Illuminate\Http\Request;
+
+class QCAdminUsersController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum');
+        $this->middleware('is_admin');
+    }
+
+    public function getAdminUsers(Request $request)
+    {
+        try {
+            return Admin::all();
+        } catch (Exception $ex) {
+            return response()->json(['Message' => 'Could fetch users: ' . $ex->getMessage()], 500);
+        }
+    }
+}
