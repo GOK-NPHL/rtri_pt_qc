81b2eb195857c9d6be2415be8c5a7850026deed5
duncan <duncanndiithi@yahoo.com>
2021-07-01 17:07:49 +0300

    list labas

--- resources/js/user/qc/admin/ListLab.js
+++ resources/js/user/qc/admin/ListLab.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LineGraph from '../../../components/utils/charts/LineGraph';
-import RTCard from '../../../components/utils/RTCard';
-import StackedHorizontal from '../../../components/utils/charts/StackedHorizontal'
-import { FetchSubmissions } from '../../../components/utils/Helpers';
 import { v4 as uuidv4 } from 'uuid';
+import Pagination from "react-js-pagination";
+import { FetchParticipantList } from '../../../components/utils/Helpers';
 
 
 class ListLab extends React.Component {
@@ -12,26 +10,168 @@ class ListLab extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            submissions: [],
-            isSubmitResult: false,
-            dtObject: null
+            data: [],
+            currElementsTableEl: [],
+            allTableElements: [],
+            selectedElement: null,
+            allowedPermissions: [],
+            userActionState: 'userList',
+            startTableData: 0,
+            endeTableData: 10,
+            activePage: 1,
         }
+        this.handlePageChange = this.handlePageChange.bind(this)
     }
 
     componentDidMount() {
 
         (async () => {
-            let response = await FetchSubmissions();
+            let response = await FetchParticipantList();
+            this.setState({
+                data: response
+            });
+            console.log(response);
         })();
 
     }
 
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        let pgNumber = pageNumber * 10 + 1;
+        this.setState({
+            startTableData: pgNumber - 11,
+            endeTableData: pgNumber - 1,
+            activePage: pageNumber
+        });
+    }
+
+    updatedSearchItem(currElementsTableEl) {
+        this.setState({
+            currElementsTableEl: currElementsTableEl,
+            activePage: 1,
+            startTableData: 0,
+            endeTableData: 10,
+        })
+    }
 
     render() {
+        const imgStyle = {
+            width: "100%"
+        };
+
+        const rowStle = {
+            marginBottom: "5px"
+        };
+
+        let tableElem = [];
+
+        if (this.state.data.length > 0) {
+
+            this.state.data.map((element, index) => {
+
+                tableElem.push(<tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{element.mfl_code}</td>
+                    <td>{element.lab_name}</td>
+                    <td>{element.email}</td>
+                    <td>{element.is_active ? 'True' : 'False'}</td>
+
+                    {
+
+                        <td>
+
+                            <a
+                                onClick={
+                                    () => {
+                                        this.toggleDisplay();
+                                        this.setState({
+                                            userActionState: 'edit',
+                                            selectedElement: element
+                                        });
+                                    }
+                                }
+                                style={{ 'marginRight': '5px' }}
+                                className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                                <i className="fas fa-user-edit"></i>
+                            </a>
+                            {/* <a
+                                onClick={() => {
+                                    this.setState({
+                                        selectedElement: element
+                                    });
+                                    $('#deleteConfirmModal').modal('toggle');
+                                }} className="d-none d-sm-inline-block btn btn-sm btn-danger shadow-sm">
+                                <i className="fas fa-user-times"></i>
+                            </a> */}
+
+                        </td>
+                    }
+
+                </tr>
+                );
+            });
+            if (this.state.allTableElements.length == 0) {
+                this.setState({
+                    allTableElements: tableElem,
+                    currElementsTableEl: tableElem
+                })
+            }
+
+        }
+
+
+        let pageContent = <div id='user_table' className='row'>
+            <div className="col-sm-12 mb-3 mt-3">
+                <h3 className="float-left">Participants List</h3>
+                <a style={{ "color": "white" }} type="button" href="add-lab" className="btn btn-primary float-right">Add Participant</a>
+            </div>
+            <div className='col-sm-12 col-md-12'>
+                <div className="form-group mb-2">
+                    <input type="text"
+                        onChange={(event) => {
+                            console.log(this.state.allTableElements);
+                            let currElementsTableEl = this.state.allTableElements.filter(elemnt =>
+                                elemnt['props']['children'][1]['props']['children'].toString().toLowerCase().trim().includes(event.target.value.trim().toLowerCase()) ||
+                                elemnt['props']['children'][2]['props']['children'].toLowerCase().trim().includes(event.target.value.trim().toLowerCase()) ||
+                                elemnt['props']['children'][3]['props']['children'].toLowerCase().trim().includes(event.target.value.trim().toLowerCase())
+                            );
+                            this.updatedSearchItem(currElementsTableEl);
+                        }}
+                        className="form-control" placeholder="search participant"></input>
+                </div>
+
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Facility Code</th>
+                            <th scope="col">Lab Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.currElementsTableEl.slice(this.state.startTableData, this.state.endeTableData)}
+                    </tbody>
+
+                </table>
+                <br />
+                <Pagination
+                    itemClass="page-item"
+                    linkClass="page-link"
+                    activePage={this.state.activePage}
+                    itemsCountPerPage={10}
+                    totalItemsCount={this.state.currElementsTableEl.length}
+                    pageRangeDisplayed={5}
+                    onChange={this.handlePageChange.bind(this)}
+                />
+            </div>
+        </div>;
 
         return (
             <React.Fragment>
-                alert(AddLab);
+                {pageContent}
             </React.Fragment>
         );
     }
