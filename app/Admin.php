9b9eee2f7038c21581eef1d7800a237793d784db
duncan <duncanndiithi@yahoo.com>
2021-07-01 13:22:39 +0300

    add field to fillable

--- app/Admin.php
+++ app/Admin.php
@@ -14,7 +14,7 @@ class Admin extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'is_admin'
+        'name', 'email', 'password', 'is_admin','phone_number'
     ];
 
     /**
