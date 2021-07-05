0837d4a623891c495e2131e603a9d19da4ef628c
duncan <duncanndiithi@yahoo.com>
2021-07-05 14:50:29 +0300

    readiness qst migration

---
+++ database/migrations/2021_07_05_103114_create_readiness_questions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReadinessQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('readiness_questions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('readiness_id');
+            $table->string('question');
+            $table->string('answer_options')->nullable();
+            $table->string('answer_type')->nullable();
+            $table->integer('qustion_position');
+            $table->string('qustion_type');
+        });
+
+        Schema::table('readiness_questions', function ($table) {
+            $table->foreign('readiness_id')
+                ->references('id')->on('readinesses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('readiness_questions');
+    }
+}
