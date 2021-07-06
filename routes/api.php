da02ec53d8245c8329af4bdd0e12b95756cd017a
duncan <duncanndiithi@yahoo.com>
2021-07-06 22:51:20 +0300

    pt shoipment add endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ParticipantController;
 use App\Http\Controllers\PT\PTReadinessController;
+use App\Http\Controllers\PT\PTShipmentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,12 +32,14 @@ Route::get('/get_admin_users', 'QC\QCAdminUsersController@getAdminUsers');
 Route::post('create_admin', 'AdminAuthController@create');
 
 
-Route::get('/get_participants',[ParticipantController::class, 'getParticipants'])->name('get_participants')->middleware('auth:admin');
-Route::post('/create_participant',[ParticipantController::class, 'createParticipant'])->name('create_participant')->middleware('auth:admin');
+Route::get('/get_participants', [ParticipantController::class, 'getParticipants'])->name('get_participants')->middleware('auth:admin');
+Route::post('/create_participant', [ParticipantController::class, 'createParticipant'])->name('create_participant')->middleware('auth:admin');
 
-Route::get('/get_lab_personel',[ParticipantController::class, 'getLabPersonel'])->name('get_lab_personel')->middleware('auth:admin');
-Route::post('/create_lab_personel',[ParticipantController::class, 'createLabPersonel'])->name('create_lab_personel')->middleware('auth:admin');
+Route::get('/get_lab_personel', [ParticipantController::class, 'getLabPersonel'])->name('get_lab_personel')->middleware('auth:admin');
+Route::post('/create_lab_personel', [ParticipantController::class, 'createLabPersonel'])->name('create_lab_personel')->middleware('auth:admin');
 
-Route::get('/get_readiness',[PTReadinessController::class, 'getReadiness'])->name('get_readiness')->middleware('auth:admin');
-Route::post('/create_readiness',[PTReadinessController::class, 'saveReadiness'])->name('create_readiness')->middleware('auth:admin');
+Route::get('/get_readiness', [PTReadinessController::class, 'getReadiness'])->name('get_readiness')->middleware('auth:admin');
+Route::post('/create_readiness', [PTReadinessController::class, 'saveReadiness'])->name('create_readiness')->middleware('auth:admin');
 
+Route::get('/get_shipment', [PTShipmentController::class, 'getShipment'])->name('get_shipment')->middleware('auth:admin');
+Route::post('/create_shipment', [PTShipmentController::class, 'saveShipment'])->name('create_shipment')->middleware('auth:admin');
