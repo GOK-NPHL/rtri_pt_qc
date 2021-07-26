4d68f10815f1122ecbe8292bd189cd8ddc0b2492
duncan <duncanndiithi@yahoo.com>
2021-07-26 15:45:19 +0300

    acess authorization

--- resources/views/user/pt/participant/dashboard.blade.php
+++ resources/views/user/pt/participant/dashboard.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <div class="container-fluid">
+
+    @if (Auth::user()->can('view_pt_component'))
     <div id="participant-pt-dashboard"></div>
+    @endif
+
 </div>
 @endsection
--- resources/views/user/qc/participant/dashboard.blade.php
+++ resources/views/user/qc/participant/dashboard.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <div class="container-fluid">
+
+    @if (Auth::user()->can('view_qc_component'))
     <div id="dashboard"></div>
+    @endif
+
 </div>
 @endsection
