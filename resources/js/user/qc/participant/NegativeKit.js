f2b586bc7e2c84e9677b85b078410653840edf9f
duncan <duncanndiithi@yahoo.com>
2021-08-27 15:51:47 +0300

    repeat test

--- resources/js/user/qc/participant/NegativeKit.js
+++ resources/js/user/qc/participant/NegativeKit.js
@@ -13,16 +13,16 @@ class NegativeKit extends React.Component {
     }
 
     render() {
-
+console.log(this.props.isShowNegativeRepeat);
         return (
             <React.Fragment>
 
                 <tr>
                     <td>QC -Negative</td>
-                    <td ><input onClick={()=>this.props.resultNegative(event)} value="c" type="checkbox" /></td>
-                    <td ><input onClick={()=>this.props.resultNegative(event)} value="v" type="checkbox" /></td>
-                    <td ><input onClick={()=>this.props.resultNegative(event)} value="lt" type="checkbox" /></td>
-                    <td onChange={()=>this.props.qcInterpretationNegative(event)}>
+                    <td ><input onClick={() => this.props.resultNegative(event)} value="c" type="checkbox" /></td>
+                    <td ><input onClick={() => this.props.resultNegative(event)} value="v" type="checkbox" /></td>
+                    <td ><input onClick={() => this.props.resultNegative(event)} value="lt" type="checkbox" /></td>
+                    <td onChange={() => this.props.qcInterpretationNegative(event)}>
                         <div className="form-check form-check-inline">
                             <input className="form-check-input" type="radio" value="lt"
                                 name="negative-radio" id="result_lt" />
@@ -52,7 +52,17 @@ class NegativeKit extends React.Component {
                             </label>
                         </div>
                     </td>
+
+                    {
+                        this.props.isShowNegativeRepeat ?
+                            <td>
+                                <button onClick={(event) => this.props.repeatNegativeTest(event)} type="button" className="btn btn-sm btn-outline-primary">Repeat</button>
+                            </td> :
+                            <td style={{ "display": "none" }}></td>
+                    }
+
                 </tr>
+
             </React.Fragment>
         );
     }
