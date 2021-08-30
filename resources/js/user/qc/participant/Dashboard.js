4e2a67e4ca97b4022d1f0d58b6eca192e7dba7c2
duncan <duncanndiithi@yahoo.com>
2021-08-30 15:09:19 +0300

    remove edit icon

--- resources/js/user/qc/participant/Dashboard.js
+++ resources/js/user/qc/participant/Dashboard.js
@@ -118,12 +118,12 @@ class Dashboard extends React.Component {
                         <td>{element['kit_lot_no']}</td>
                         <td>{element['testing_date']}</td>
                         <td>
-                            <a
+                            {/* <a
                                 href="#"
                                 style={{ "display": "inlineBlock", 'marginRight': '5px' }}
                                 className="d-none d-sm-inline-block btn btn-sm btn-info shadow-sm">
                                 <i className="fas fa-user-edit"></i>
-                            </a>
+                            </a> */}
                             <a
                                 onClick={() => this.deleteSubmissionHandler(element['id'])}
                                 style={{ "display": "inlineBlock" }}
